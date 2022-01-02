65e51b68ad7441437508e9ef2a3813e6414c19ce
JaeseokWoo <JaeseokW@gmail.com>
2022-01-02 21:59:08 +0900

    feat: getMetadata 함수 작성
    node-html-parser로 파싱한 dom에서 title을 추출한다.

--- routes/preview/getMetadata.js
+++ routes/preview/getMetadata.js
@@ -1,26 +1,49 @@
-const axios = require('axios');
-const parse = require('node-html-parser');
+const axios = require("axios");
+const parse = require("node-html-parser");
+
+const getTitle = (dom) => {
+  const ogTitle = dom.querySelector('meta[property="og:title"]');
+  if (ogTitle !== null) {
+    const content = ogTitle.getAttribute("content");
+    if (content) {
+      return content;
+    }
+  }
+
+  const twitterTitle = dom.querySelector('meta[name="twitter:title"]');
+  if (twitterTitle !== null) {
+    const content = twitterTitle.getAttribute("content");
+    if (content) {
+      return content;
+    }
+  }
+
+  const title = dom.querySelector("title");
+  if (title) {
+    return title.text;
+  }
+  return null;
+};
 
 const config = {
-    headers: {
-        'Accept-Encoding': 'gzip,deflate,br', 
-    }
-}
+  headers: {
+    "Accept-Encoding": "gzip,deflate,br",
+  },
+};
 
 const getMetadata = async (url) => {
-    try {
-        const { data } = await axios.get(url, config);
-        const html = parse(data);
+  try {
+    const { data } = await axios.get(url, config);
+    const html = parse(data);
 
-        const title = getTitle(html);
-        const description = getDescription(html);
+    const title = getTitle(html);
+    const description = getDescription(html);
 
-        return { title, description };
-
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
-}
+    return { title, description };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 module.exports = getMetadata;
