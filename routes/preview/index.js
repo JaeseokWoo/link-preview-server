0ad15ad8e280220efc8c6ee89771736ca47a6395
JaeseokWoo <JaeseokW@gmail.com>
2022-01-02 21:45:17 +0900

    feat: preview get api 작성

--- routes/preview/index.js
+++ routes/preview/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 
-
 const previewRouter = express.Router();
+
+previewRouter.get('/', async (req, res, next) => {
+    let { url } = req.query;
+    if (!url) {
+        return next(new Error("no url"));
+    }
+    url = url.indexOf('://') === -1 ? 'http://' + url : url;
+    const data = await getMeatadata(url);
+    res.send(data);
+});
 
 module.exports = previewRouter;
