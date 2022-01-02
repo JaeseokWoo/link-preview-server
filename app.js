0d61c0e11652e553196986588a0583450cc6439a
JaeseokWoo <JaeseokW@gmail.com>
2022-01-02 21:23:58 +0900

    feat: 초기 서버 구조 생성
    link-preview에 관한 요청을 처리할 previewRouter 생성

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+
+import previewRouter from "./routes/preview/index.js";
+
+const app = express();
+app.set("port", process.env.PORT || 5000);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/preview", previewRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message;
+  res.status(status).send({
+    status,
+    message,
+  });
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
---
+++ routes/preview/index.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const previewRouter = Router();
+
+module.exports = previewRouter;
