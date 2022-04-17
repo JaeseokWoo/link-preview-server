9a3eb2760214c75713a207f7ae37af2305ce21ea
JaeseokWoo <JaeseokW@gmail.com>
2022-04-17 22:16:53 +0900

    FEAT: 응답받은 metadata 출력 UI 구현

--- front-end/src/search/component/Response.tsx
+++ front-end/src/search/component/Response.tsx
@@ -8,13 +8,28 @@ export type Metadata = {
   domain: string | null;
 };
 
+function transformPrettyJsonStringify(json: { [key: string]: string | null }) {
+  return Object.keys(json).reduce(
+    (acc: string, key: string, idx: number, origin: string[]) => {
+      let ret = `${acc}\n\t${key}: ${json[key]}`;
+      if (idx === origin.length - 1) {
+        ret += '\n}';
+      } else {
+        ret += ',';
+      }
+      return ret;
+    },
+    '{'
+  );
+}
+
 export default function Response({ metadata }: { metadata: Metadata }) {
   return (
     <TextField
       disabled
       fullWidth
       multiline
-      value={JSON.stringify(metadata).replaceAll(',', ',\n')}
+      value={transformPrettyJsonStringify(metadata)}
     />
   );
 }
