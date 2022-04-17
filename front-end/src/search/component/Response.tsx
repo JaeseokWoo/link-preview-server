3739683ca1374d42c91fd813d5f313071a6e0bf5
JaeseokWoo <JaeseokW@gmail.com>
2022-04-17 22:33:34 +0900

    FIX: 응답받은 metadata 출력 방법 수정
    json 형식으로 알맞게 출력하는 방법을 알게되어 수정

--- front-end/src/search/component/Response.tsx
+++ front-end/src/search/component/Response.tsx
@@ -8,28 +8,13 @@ export type Metadata = {
   domain: string | null;
 };
 
-function transformPrettyJsonStringify(json: { [key: string]: string | null }) {
-  return Object.keys(json).reduce(
-    (acc: string, key: string, idx: number, origin: string[]) => {
-      let ret = `${acc}\n\t${key}: ${json[key]}`;
-      if (idx === origin.length - 1) {
-        ret += '\n}';
-      } else {
-        ret += ',';
-      }
-      return ret;
-    },
-    '{'
-  );
-}
-
 export default function Response({ metadata }: { metadata: Metadata }) {
   return (
     <TextField
       disabled
       fullWidth
       multiline
-      value={transformPrettyJsonStringify(metadata)}
+      value={JSON.stringify(metadata, null, 2)}
     />
   );
 }
