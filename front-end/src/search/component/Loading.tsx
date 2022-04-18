c3b44ddcb2f84d1461ab86ceedb4ffba60d38de1
JaeseokWoo <JaeseokW@gmail.com>
2022-04-18 22:49:59 +0900

    STYLE: CSS 작업
    로딩 크기 증가
    margin 추가

--- front-end/src/search/component/Loading.tsx
+++ front-end/src/search/component/Loading.tsx
@@ -4,8 +4,8 @@ import Box from '@mui/material/Box';
 
 export default function Loading() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
+    <Box sx={{ m: 3, display: 'flex', justifyContent: 'center' }}>
+      <CircularProgress size={60} />
     </Box>
   );
 }
