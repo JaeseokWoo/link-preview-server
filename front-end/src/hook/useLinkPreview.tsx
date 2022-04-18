ee23c39a4458e44f9cc91a0ef2ac9236f5db3f10
JaeseokWoo <JaeseokW@gmail.com>
2022-04-18 22:05:00 +0900

    FEAT: useLinkPreview 구현

--- front-end/src/hook/useLinkPreview.tsx
+++ front-end/src/hook/useLinkPreview.tsx
@@ -1,1 +1,25 @@
-export default function useLinkPreview(url: string) {}
+import { useState, useEffect } from 'react';
+import { Metadata } from '../search/component/LinkPreview';
+
+async function getMetadata(url: string) {
+  const res = await fetch(`http://localhost:5000/api/link-preview/?url=${url}`);
+  const metadata = await res.json();
+  return metadata;
+}
+
+export default function useLinkPreview(url: string) {
+  const [error, setError] = useState<null | Error>(null);
+  const [metadata, setMetadata] = useState<null | Metadata>(null);
+
+  useEffect(() => {
+    getMetadata(url)
+      .then(res => {
+        setMetadata(res);
+      })
+      .catch(err => {
+        setError(err);
+      });
+  }, [url]);
+
+  return { metadata, isLoading: !error && !metadata, isError: error };
+}
