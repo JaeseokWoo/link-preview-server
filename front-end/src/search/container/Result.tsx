d52f92143c324a33c88eece61641e722333224c4
JaeseokWoo <JaeseokW@gmail.com>
2022-04-18 22:39:35 +0900

    FIX: useLinkPreview, Loading, Error component 사용

--- front-end/src/search/container/Result.tsx
+++ front-end/src/search/container/Result.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import LinkPreview, { Metadata } from '../component/LinkPreview';
+import LinkPreview from '../component/LinkPreview';
 import Response from '../component/Response';
+import Loading from '../component/Loading';
+import Error from '../component/Error';
+import useLinkPreview from '../../hook/useLinkPreview';
 
-export default function Result({ metadata }: { metadata: Metadata }) {
+export default function Result({ url }: { url: string }) {
+  const { metadata, isLoading, isError } = useLinkPreview(url);
   return (
     <>
-      {metadata && <LinkPreview metadata={metadata} />}
-      {metadata && <Response metadata={metadata} />}
+      {isLoading && <Loading />}
+      {isError && <Error error={isError} />}
+      {metadata && (
+        <>
+          <LinkPreview metadata={metadata} />
+          <Response metadata={metadata} />
+        </>
+      )}
     </>
   );
 }
