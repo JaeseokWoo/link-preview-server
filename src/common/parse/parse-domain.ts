52a455cdfe10d42647b06b2e88ec4848c61af4e1
JaeseokWoo <JaeseokW@gmail.com>
2022-04-21 22:58:46 +0900

    FIX: domain 파싱하는 곳 수정

--- src/common/parse/parse-domain.ts
+++ src/common/parse/parse-domain.ts
@@ -3,7 +3,10 @@ import { HTMLElement } from 'node-html-parser';
 const parseDomain = (html: HTMLElement, url: string) => {
   const canonical = html.querySelector('link[rel=canonical]');
   if (canonical?.getAttribute('href')) {
-    return canonical.getAttribute('href');
+    const content = canonical.getAttribute('href');
+    if (content) {
+      return new URL(content).hostname.replace('www', '');
+    }
   }
 
   const ogUrl = html.querySelector('meta[property="og:url"]');
