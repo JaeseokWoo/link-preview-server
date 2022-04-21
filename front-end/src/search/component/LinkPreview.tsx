43899e6806724dcaa4c5df35795ca83692d78255
JaeseokWoo <JaeseokW@gmail.com>
2022-04-21 23:14:31 +0900

    STYLE: MUI import 방법 통일

--- front-end/src/search/component/LinkPreview.tsx
+++ front-end/src/search/component/LinkPreview.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import CardActionArea from '@mui/material/CardActionArea';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+
 import logo from '../../assets/logo.png';
 
 export type Metadata = {
